25acd463c8095419d91a6f1d94638e7d6f3b3168
Shoichi Kagawa <e411z7t40w@gmail.com>
2022-04-22 04:19:00 +0000

    Fix open

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+if command -v zsh &> /dev/null
+then
+    user=$(whoami)
+    sudo chsh -s $(which zsh) $user
+fi
+
 mkdir -p ~/.vscode-remote/data/Machine/
 mv settings.json ~/.vscode-remote/data/Machine/
 
 mv .zshrc ~/
 
 mkdir ~/bin
-curl -L -o ~/bin/open https://raw.githubusercontent.com/superbrothers/opener/master/bin/open
+mv open ~/bin/open
 chmod 755 ~/bin/open
-# xdg-open command
-curl -L -o ~/bin/xdg-open https://raw.githubusercontent.com/superbrothers/opener/master/bin/xdg-open
-chmod 755 ~/bin/xdg-open
+
 # Add ~/bin to $PATH and enable it
 echo 'export PATH="$HOME/bin:$PATH"' >> ~/.zshrc
 source ~/.zshrc
